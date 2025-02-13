4e2985e664388306d515beaead8d69cdb94bb43b
Estebanr32 <juanes020609@gmail.com>
2025-02-13 16:23:07 -0500

    Inicio de la calculadora

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print ("Hola, bienvenido a Calculator.git")
+
+input("Ingrese la operación que desea usar:\n1.Suma\n2.Resta\n3.Multiplicación\n4.División")
+result
+
+def suma (a,b):
+    result = a + b
+    return result
+
+def resta (a,b):
+    result = a - b
+    return result
