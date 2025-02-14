24e8b3df87d3f4a74e4eb5ecace930152ef9bae4
Svelezmo <sebasvm59@gmail.com>
2025-02-14 14:29:15 -0500

    avance calculadora

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 print ("Hola, bienvenido a Calculator.git")
 
 input("Ingrese la operación que desea usar:\n1.Suma\n2.Resta\n3.Multiplicación\n4.División")
-result
 
 def suma (a,b):
     result = a + b
@@ -10,3 +9,13 @@ def suma (a,b):
 def resta (a,b):
     result = a - b
     return result
+
+def multiplicacion (a,b):
+    result = a*b
+    return result 
+
+def division (a,b):
+    if(denominador == 0):
+        print("Resultado indeterminado")
+    result = a/b
+    
